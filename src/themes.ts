3042662665f7c9ab00afb04fb9dad6944c1df5aa
Henry Bley-Vroman <olets@olets.dev>
2024-10-22 12:53:21 -1000

    feat(themes): bring up to date with shikijs/textmate-grammars-themes

--- src/themes.ts
+++ src/themes.ts
@@ -1,11 +1,29 @@
 export type Theme =
+  | 'andromeeda'
+  | 'aurora-x'
+  | 'ayu-dark'
+  | 'catppuccin-frappe'
+  | 'catppuccin-latte'
+  | 'catppuccin-macchiato'
+  | 'catppuccin-mocha'
   | 'css-variables'
   | 'dark-plus'
   | 'dracula-soft'
   | 'dracula'
+  | 'everforest-dark'
+  | 'everforest-light'
+  | 'github-dark-default'
   | 'github-dark-dimmed'
+  | 'github-dark-high-contrast'
   | 'github-dark'
+  | 'github-light-default'
+  | 'github-light-high-contrast'
   | 'github-light'
+  | 'houston'
+  | 'kanagawa-dragon'
+  | 'kanagawa-lotus'
+  | 'kanagawa-wave'
+  | 'laserwave'
   | 'light-plus'
   | 'material-theme-darker'
   | 'material-theme-lighter'
@@ -15,28 +33,54 @@ export type Theme =
   | 'min-dark'
   | 'min-light'
   | 'monokai'
+  | 'night-owl'
   | 'nord'
   | 'one-dark-pro'
+  | 'one-light'
+  | 'plastic'
   | 'poimandres'
+  | 'red'
   | 'rose-pine-dawn'
   | 'rose-pine-moon'
   | 'rose-pine'
   | 'slack-dark'
   | 'slack-ochin'
+  | 'snazzy-light'
   | 'solarized-dark'
   | 'solarized-light'
+  | 'synthwave-84'
+  | 'tokyo-night'
+  | 'vesper'
   | 'vitesse-black'
   | 'vitesse-dark'
   | 'vitesse-light'
 
 export const themes: Theme[] = [
+  'andromeeda',
+  'aurora-x',
+  'ayu-dark',
+  'catppuccin-frappe',
+  'catppuccin-latte',
+  'catppuccin-macchiato',
+  'catppuccin-mocha',
   'css-variables',
   'dark-plus',
   'dracula-soft',
   'dracula',
+  'everforest-dark',
+  'everforest-light',
+  'github-dark-default',
   'github-dark-dimmed',
+  'github-dark-high-contrast',
   'github-dark',
+  'github-light-default',
+  'github-light-high-contrast',
   'github-light',
+  'houston',
+  'kanagawa-dragon',
+  'kanagawa-lotus',
+  'kanagawa-wave',
+  'laserwave',
   'light-plus',
   'material-theme-darker',
   'material-theme-lighter',
@@ -46,16 +90,24 @@ export const themes: Theme[] = [
   'min-dark',
   'min-light',
   'monokai',
+  'night-owl',
   'nord',
   'one-dark-pro',
+  'one-light',
+  'plastic',
   'poimandres',
+  'red',
   'rose-pine-dawn',
   'rose-pine-moon',
   'rose-pine',
   'slack-dark',
   'slack-ochin',
+  'snazzy-light',
   'solarized-dark',
   'solarized-light',
+  'synthwave-84',
+  'tokyo-night',
+  'vesper',
   'vitesse-black',
   'vitesse-dark',
   'vitesse-light'
