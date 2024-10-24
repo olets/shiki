9fd7d38c74b8ca70c99dc0ba2ff3029807581666
Henry Bley-Vroman <olets@olets.dev>
2024-10-24 08:35:51 -1000

    feat(eleventy-plugin-shiki-twoslash): use ts, build, add build. adds @11ty/eleventy...
    >>> bun add @11ty/eleventy

---
+++ packages/eleventy-plugin-shiki-twoslash/dist/index.d.ts
@@ -0,0 +1,5 @@
+/**
+ * @param {import("@11ty/eleventy").UserConfig} eleventyConfig
+ * @param {import("@olets/shiki-twoslash").UserConfigSettings} options
+ */
+export default function (eleventyConfig: any, options?: {}): Promise<void>;
--- packages/eleventy-plugin-shiki-twoslash/src/index.ts
+++ packages/eleventy-plugin-shiki-twoslash/src/index.ts
@@ -3,13 +3,13 @@ import {
   transformAttributesToHTML,
 } from "@olets/remark-shiki-twoslash";
 
+// H/t https://github.com/shikijs/twoslash/issues/193#issue-2056662905
+
 /**
- * @param {*} eleventyConfig
+ * @param {import("@11ty/eleventy").UserConfig} eleventyConfig
  * @param {import("@olets/shiki-twoslash").UserConfigSettings} options
  */
-
-// H/t https://github.com/shikijs/twoslash/issues/193#issue-2056662905
-export default async function (eleventyConfig, options = {}) {
+export default async function (eleventyConfig, options = {}): Promise<void> {
   const { highlighters } = await setupForFile(options);
 
   eleventyConfig.addMarkdownHighlighter((code, lang, fence) => {
